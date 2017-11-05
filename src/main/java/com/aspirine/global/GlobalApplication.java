5682cea1c1c67d3544c1c93699ad9053c7b4360a
yathish <yathish.jayaram@airfi.aero>
2017-11-05 15:15:02 +0530

    Fix auto imports

--- src/main/java/com/aspirine/global/GlobalApplication.java
+++ src/main/java/com/aspirine/global/GlobalApplication.java
@@ -2,12 +2,18 @@ package com.aspirine.global;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+/**
+ * Main application to run the project.
+ *
+ * @author yathish.
+ */
 @SpringBootApplication
 public class GlobalApplication {
 
 	public static void main(String[] args) {
-		org.springframework.context.ApplicationContext context = SpringApplication.run(GlobalApplication.class, args);
+		ApplicationContext context = SpringApplication.run(GlobalApplication.class, args);
 
 		//Spring by default will recognize bean name from class name with camel case word
 		HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
--- src/main/java/com/aspirine/global/HelloWorld.java
+++ src/main/java/com/aspirine/global/HelloWorld.java
@@ -1,11 +1,18 @@
 package com.aspirine.global;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * Sample hello world class.
+ *
+ * @author yathish.
+ */
 @Component
 public class HelloWorld {
 
-    org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(HelloWorld.class);
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorld.class);
 
     public void getHelloWorld() {
         LOG.info(" Log message ---> Hello World :) ");
